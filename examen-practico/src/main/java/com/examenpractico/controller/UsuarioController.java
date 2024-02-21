38c04ff99e430a1c2f24d482bcee02d0a1418e03
agcudco <agcudco@espe.edu.ec>
2024-02-21 13:36:14 -0500

    tabla curso

--- examen-practico/src/main/java/com/examenpractico/controller/UsuarioController.java
+++ examen-practico/src/main/java/com/examenpractico/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.examenpractico.exception.ResourceNotFoundException;
 import com.examenpractico.model.Usuario;
 import com.examenpractico.repository.UsuarioRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
